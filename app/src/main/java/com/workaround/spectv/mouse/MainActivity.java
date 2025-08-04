ab1c6e22040bd9f6128b372dd4e6d22748a0c86f
Isaac Schemm <isaacschemm@gmail.com>
2025-08-03 19:34:55 -0500

    Fix hiding seek bar for DVR recordings

--- app/src/main/java/com/workaround/spectv/mouse/MainActivity.java
+++ app/src/main/java/com/workaround/spectv/mouse/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends FragmentActivity  {
                      document.querySelector('.btn-success')?.click();
                      // Hide certain elements
                      $('<style>volume-control, toggle-fullscreen { display:none !important; }</style>').appendTo(document.body);
-                     $('<style id=mouseNotMovedStyles>nav.top-level-nav, main-menu li:not(:has(#livetv-link), :has(#watchAnytime-link)), global-search, ovp-scrubber .slider { display:none; }</style>').appendTo(document.body);
+                     $('<style id=mouseNotMovedStyles>nav.top-level-nav, main-menu li:not(:has(#livetv-link), :has(#watchAnytime-link)), global-search, ovp-scrubber .slider { display:none !important; }</style>').appendTo(document.body);
                      document.body.addEventListener('mousemove', () => {
                         $('#mouseNotMovedStyles').remove();
                      }, { once: true });
